0ca1649bd6259c4ca7c2c35ae3bc5decac0fbc36
no9878776_glb <no9878776@gmail.com>
2020-11-26 17:23:29 +0800

    edit on branch hot_fix

--- src/main/java/com/atguigu/git/Happy.java
+++ src/main/java/com/atguigu/git/Happy.java
@@ -4,5 +4,6 @@ public class Happy {
 	public static void main(String[] args) {
 		System.out.println("Test....");
 		System.out.println("more Test...Right1");
+		System.out.println("hot fix ...");
 	}
 }
